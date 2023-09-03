086988b367c241b5e37909a8f63765cc574737a3
john <johnmaluki58@gmail.com>
2023-09-03 22:21:44 +0300

    Create tests Aggregate and Relationship Methods

--- app/tests/test_customer.py
+++ app/tests/test_customer.py
@@ -12,10 +12,64 @@ class TestCustomer:
 
         assert customer != None
         assert customer.first_name == "John"
+    
+    def test_full_name(self):
+        """
+            Test returns the full name of the customer, with the first name 
+            and the last name  concatenated, Western style.
+        """
+        customer = Customer(first_name="John", last_name="Doe")
+
+        assert customer.full_name() == "John Doe"
+
+    def test_favorite_restaurant(self):
+        """
+           Test returns the restaurant instance that has the highest star rating from this customer
+        """
+        engine = create_engine(SQLITE_URL)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        clear_db(session)
+
+        customer = Customer(first_name="John", last_name="Doe")
+        customer2 = Customer(first_name="John1", last_name="Doe1")
+        session.add(customer)
+        session.add(customer2)
+
+        session.commit()
+
+        customer = session.query(Customer).first()
+        customer2 = session.query(Customer).all()[1]
+
+        restaurant1 = Restaurant(name="R1", price=1000)
+        restaurant2 = Restaurant(name="R2", price=2000)
+        session.bulk_save_objects([restaurant1, restaurant2])
+
+        session.commit()
+
+        restaurant1 = session.query(Restaurant).first()
+        restaurant2 = session.query(Restaurant).all()[1]
+
+        review1c1 = Review(star_rating=5, customer_id=customer.id, restaurant_id=restaurant1.id)
+        review2c1 = Review(star_rating=8, customer_id=customer.id, restaurant_id=restaurant2.id)
+        review1c2 = Review(star_rating=6, customer_id=customer2.id, restaurant_id=restaurant1.id)
+        review2c2 = Review(star_rating=10, customer_id=customer2.id, restaurant_id=restaurant2.id)
+
+        session.bulk_save_objects([review1c1, review2c1, review1c2, review2c2])
+        session.commit()
+
+        customer = session.query(Customer).first()
+        customer2 = session.query(Customer).all()[1]
+
+        assert customer.favorite_restaurant().name == "R2"
+        assert customer.favorite_restaurant().price == 2000
+
+        clear_db(session)
 
     def test_get_reviews(self):
         """
-            Should return a collection of all the reviews that the `Customer` has left
+            Test returns a collection of all the reviews that the `Customer` has left
         """
         engine = create_engine(SQLITE_URL)
         Session = sessionmaker(bind=engine)
@@ -60,7 +114,7 @@ class TestCustomer:
 
     def test_get_restaurants(self):
         """
-            should return a collection of all the restaurants that the `Customer` has 
+            Test returns a collection of all the restaurants that the `Customer` has 
             reviewed
         """
         engine = create_engine(SQLITE_URL)
@@ -103,6 +157,90 @@ class TestCustomer:
         assert len(customer2.get_restaurants()) == 2
 
         clear_db(session)
+
+    def test_add_review(self):
+        """
+            Test takes a `restaurant` (an instance of the `Restaurant` class) and a rating and
+            creates a new review for the restaurant with the given `restaurant_id`
+        """
+        engine = create_engine(SQLITE_URL)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        clear_db(session)
+
+        customer = Customer(first_name="John", last_name="Doe")
+        session.add(customer)
+        session.commit()
+
+        restaurant = Restaurant(name="R1", price=1000)
+        session.add(restaurant)
+        session.commit()
+
+        restaurant = session.query(Restaurant).first()
+
+        
+        customer = session.query(Customer).first()
+        customer.add_review(restaurant, 9)
+
+        review = session.query(Review).all()[0]
+
+        assert review.get_customer().first_name == "John"
+        assert review.get_customer().last_name == "Doe"
+        assert review.get_restaurant().name == "R1"
+
+        clear_db(session)
+
+    def test_delete_reviews(self):
+        """
+            Test takes a `restaurant` (an instance of the `Restaurant` class) and
+            removes **all** their reviews for this restaurant you will have to delete rows 
+            from the `reviews` table to get this to work!
+        """
+        engine = create_engine(SQLITE_URL)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        clear_db(session)
+
+        customer = Customer(first_name="John", last_name="Doe")
+        customer2 = Customer(first_name="John1", last_name="Doe1")
+        session.add(customer)
+        session.add(customer2)
+
+        session.commit()
+
+        customer = session.query(Customer).first()
+        customer2 = session.query(Customer).all()[1]
+
+        restaurant1 = Restaurant(name="R1", price=1000)
+        restaurant2 = Restaurant(name="R2", price=2000)
+        session.bulk_save_objects([restaurant1, restaurant2])
+
+        session.commit()
+
+        restaurant1 = session.query(Restaurant).first()
+        restaurant2 = session.query(Restaurant).all()[1]
+
+        review1c1 = Review(star_rating=5, customer_id=customer.id, restaurant_id=restaurant1.id)
+        review1c2 = Review(star_rating=6, customer_id=customer.id, restaurant_id=restaurant1.id)
+        review2c2 = Review(star_rating=10, customer_id=customer2.id, restaurant_id=restaurant2.id)
+
+        session.bulk_save_objects([review1c1, review1c2, review2c2])
+        session.commit()
+
+        customer = session.query(Customer).first()
+        customer2 = session.query(Customer).all()[1]
+
+        customer.delete_reviews(restaurant1)
+
+        assert len(customer.get_reviews()) == 0
+        assert len(customer2.get_reviews()) == 1
+
+        clear_db(session)
+
+        
+
 
 
 
